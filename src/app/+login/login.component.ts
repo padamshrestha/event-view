e500ee5a7082b902635624ec74e3211497b69bee
John Papa <john@johnpapa.net>
2016-07-01 22:03:19 -0400

    added spinner to login

--- src/app/+login/login.component.ts
+++ src/app/+login/login.component.ts
@@ -1,24 +1,26 @@
-import { Component }   from '@angular/core';
-import { Router }      from '@angular/router';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
-import { AuthService } from '../auth';
+import {AuthService} from '../auth';
+import {SpinnerService} from '../shared';
 
-@Component({
-  moduleId: module.id,
-  templateUrl: 'login.component.html'
-})
+@Component({moduleId : module.id, templateUrl : 'login.component.html'})
 export class LoginComponent {
   message: string;
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService,
+    private router: Router,
+    private spinnerService: SpinnerService) {
     this.setMessage();
   }
 
   setMessage() {
     this.message = 'Logged ' + (this.authService.isLoggedIn ? 'in' : 'out');
+    this.spinnerService.hide();
   }
 
   login() {
+    this.spinnerService.show();
     this.message = 'Trying to log in ...';
 
     this.authService.login().subscribe(() => {
@@ -26,12 +28,13 @@ export class LoginComponent {
       if (this.authService.isLoggedIn) {
         // Todo: capture where the user was going and nav there.
         // Meanwhile redirect the user to the dashboard
-        this.router.navigate(['/dashboard']);
+        this.router.navigate([ '/dashboard' ]);
       }
     });
   }
 
   logout() {
+    this.spinnerService.show();
     this.authService.logout();
     this.setMessage();
   }

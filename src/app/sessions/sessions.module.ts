6780de0288b9b22280bfab7a29bb6649417ec704
John Papa <john@johnpapa.net>
2016-08-17 02:15:01 -0400

    // TODO: Remove this when bug is fixed in A2 RC5

--- src/app/sessions/sessions.module.ts
+++ src/app/sessions/sessions.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { SessionButtonComponent } from './shared/session-button/Session-button.component';
 import { routing, routedComponents } from './sessions.routing';
 import { SharedModule } from '../shared/shared.module';
+import { SessionService } from './shared/session.service';  // TODO: Remove this when bug is fixed in A2 RC5
 
 const declarables = [SessionButtonComponent, routedComponents]
 
 @NgModule({
   imports: [routing, SharedModule],
   declarations: [declarables],
+  providers: [SessionService] // TODO: Remove this when bug is fixed in A2 RC5
 })
 export default class SessionsModule { }

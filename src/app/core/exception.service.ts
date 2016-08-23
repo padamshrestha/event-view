93ddc9712977bc798a806af1a6b213d28f55f54c
John Papa <john@johnpapa.net>
2016-08-23 01:36:24 -0400

    extra import

--- src/app/core/exception.service.ts
+++ src/app/core/exception.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/of'; // TODO: remove
 
 import { ToastService } from './toast/toast.service';
 

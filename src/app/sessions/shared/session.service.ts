6f5329cff4e04b7ba23483d0f4f48b1ec12e0a69
John Papa <john@johnpapa.net>
2016-08-27 23:06:12 -0400

    ugh

--- src/app/sessions/shared/session.service.ts
+++ src/app/sessions/shared/session.service.ts
@@ -15,7 +15,6 @@ export class SessionService {
     private exceptionService: ExceptionService,
     private messageService: MessageService,
     private spinnerService: SpinnerService) {
-    console.log('********** created session service');
     this.messageService.state.subscribe(state => this.getSessions());
   }
 

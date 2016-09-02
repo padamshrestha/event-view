5906b70f0bcf90a0eee1c5cd47a76a2c56915e7d
John Papa <john@johnpapa.net>
2016-09-01 23:22:38 -0400

    tweaks for loading

--- src/system-config.ts
+++ src/system-config.ts
@@ -1,30 +1,58 @@
-/***********************************************************************************************
- * User Configuration.
- **********************************************************************************************/
-/** Map relative paths to URLs. */
-const map: any = {
-  'app' : 'app',
-  'main': 'main.js',
-  '@angular' : 'node_modules/@angular',
-  'angular2-in-memory-web-api' : 'node_modules/angular2-in-memory-web-api',
-  'rxjs' : 'node_modules/rxjs',
-  'ngfactory': 'ngfactory',
+declare var System;
+
+var paths = {
+  // paths serve as alias
+  'npm:': 'node_modules/'
 };
 
-// packages tells the System loader how to load when no filename and/or no
-// extension
-const packages: any = {
-  'app' : {main : 'main.js', defaultExtension : 'js'},
-  'api' : {defaultExtension : 'js'},
-  'rxjs' : {defaultExtension : 'js'},
-  'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
-  'ngfactory': {main : 'index.js', defaultExtension : 'js'},
+// map tells the System loader where to look for things
+var map = {
+  // our app is within the app folder
+  app: 'app',
+
+  // angular bundles
+  '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+  '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+  '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+  '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+  '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+  '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+  '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+  '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+
+  // angular testing umd bundles
+  '@angular/core/testing': 'npm:@angular/core/bundles/core-testing.umd.js',
+  '@angular/common/testing': 'npm:@angular/common/bundles/common-testing.umd.js',
+  '@angular/compiler/testing': 'npm:@angular/compiler/bundles/compiler-testing.umd.js',
+  '@angular/platform-browser/testing': 'npm:@angular/platform-browser/bundles/platform-browser-testing.umd.js',
+  '@angular/platform-browser-dynamic/testing': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
+  '@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
+  '@angular/router/testing': 'npm:@angular/router/bundles/router-testing.umd.js',
+  '@angular/forms/testing': 'npm:@angular/forms/bundles/forms-testing.umd.js',
+
+  // other libraries
+  'rxjs': 'npm:rxjs',
+  'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
+};
+// packages tells the System loader how to load when no filename and/or no extension
+var packages = {
+  app: {
+    main: 'main.js',
+    defaultExtension: 'js'
+  },
+  rxjs: {
+    defaultExtension: 'js'
+  },
+  'angular2-in-memory-web-api': {
+    main: './index.js',
+    defaultExtension: 'js'
+  }
 };
 
 const barrels: any = [
   // App specific barrels.
-  'app/core',
   'app/models',
+  'app/shared',
 ];
 
 barrels.forEach((barrelName: string) => {
@@ -32,9 +60,6 @@ barrels.forEach((barrelName: string) => {
 });
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
-/***********************************************************************************************
- * Everything underneath this line is managed by the CLI.
- **********************************************************************************************/
 
 const ngPackageNames: string[] = [
   'common',
@@ -68,6 +93,10 @@ var setPackageConfig = packIndex;
 // Add package entries for angular packages
 ngPackageNames.forEach(setPackageConfig);
 
-var config = {map : map, packages : packages};
+var config = {
+  paths: paths,
+  map: map,
+  packages: packages
+};
 
 System.config(config);

23c6bad5c9e909ed51be57513472e5dcd3d3a37f
John Papa <john@johnpapa.net>
2016-09-01 22:54:46 -0400

    catching up with master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,7 @@ import { LoginModule } from './login/login.module';
     // AppRoutingModule defines the catch-all ** route
     AppRoutingModule,
     CoreModule,
-    InMemoryWebApiModule.forRoot(InMemoryStoreService, { delay: 600 }),
+    // InMemoryWebApiModule.forRoot(InMemoryStoreService, { delay: 600 }),
   ],
   declarations: [AppComponent, PageNotFoundComponent],
   providers: [SpeakerService],
